04fa826de74ccf265bb95d00c7aaa363ed3ea2a2
John Royall <johnroyall7@yahoo.com>
2025-01-06 21:14:55 -0500

    step 4, pre-solution

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ text }) {
+function Guess({ text, answer }) {
   const numOfLetters = 5;
+
+  let guessCheckData = [];
+  if (text && answer) {
+    guessCheckData = checkGuess(text, answer);
+  }
+
+  function statusForIndex(index) {
+    return guessCheckData[index]['status'];
+  }
+
+  function hasBeenChecked() {
+    return !!guessCheckData.length;
+  }
 
   return (
     <p className="guess">
       {range(numOfLetters).map((num, index) => (
-        <span key={index} className="cell">
-          {text && text[index]}
+        <span
+          key={num}
+          className={`cell ${hasBeenChecked() ? statusForIndex(index) : ''}`}
+        >
+          {text && text[num]}
         </span>
       ))}
     </p>
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,11 @@ import { range } from '../../utils';
 import Guess from '../Guess';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num, index) => (
-        <Guess key={index} text={guesses[num]} />
+        <Guess key={index} text={guesses[num]} answer={answer} />
       ))}
     </div>
   );
